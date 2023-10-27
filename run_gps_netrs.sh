ad204504618057fa7581244f07f57fcc64db63cd
Roberto Toapanta <robertocarlos.toapanta@gmail.com>
2023-10-27 16:58:47 -0500

    update

--- run_gps_netrs.sh
+++ run_gps_netrs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Welcome $USER."
+echo "Welcome ${USER}."
 
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
@@ -7,14 +7,14 @@ eval "$(/$HOME/anaconda3/bin/conda shell.bash hook)"
 
 if [ $? != 0 ]
     then
-        echo "Error ejecutar proyecto1: error carga conda: CRONTAB_SCRIPT_ERROR">&2
+        echo "Error ejecutar gpsNetRsProject: error carga conda: CRONTAB_SCRIPT_ERROR">&2
 fi
 
 conda activate gps_netrs_env
 
 if [ $? != 0 ]
     then
-        echo "Error ejecutar proyecto1: error activar entorno CRONTAB_SCRIPT_ERROR">&2
+        echo "Error ejecutar gpsNetRsProject: error activar entorno CRONTAB_SCRIPT_ERROR">&2
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
